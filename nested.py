fe01857468b93a7168aee6eae293a0c9fc5c7989
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-08 14:14:29 +0500

    Assignment

---
+++ nested.py
@@ -0,0 +1,9 @@
+hasticket = True
+age = 18 
+if hasticket :
+    if age>=18:
+        print("You may sit on VIP section")
+    else:
+        print("You may sit on General Section")
+else:
+    print("No Entry")            
