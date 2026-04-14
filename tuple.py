9dcd04c0d410da92bef955e11203f4c679fb1e54
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-14 14:27:46 +0500

    Python-tuple

---
+++ tuple.py
@@ -0,0 +1,20 @@
+# num = (2,4,6,8,10)
+# print(num)
+
+# for x in num:
+#     print(x)
+
+
+# count
+num = (2,4,6,8,10,12,14,16,18,20,22,24,26,28,30)
+print(num.count(4))
+# index
+print(num.index(8))
+# calculate lenght
+print(len(num))
+# using max print maximum number
+print(max(num))
+# using min print minimum number
+print(min(num))
+# sum of numbers
+print(sum(num))
