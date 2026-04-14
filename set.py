6ef7f6caaf2dee378eb63b34f7e373f01ccb1212
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-14 14:29:11 +0500

    Add files via upload
    python-set

---
+++ set.py
@@ -0,0 +1,18 @@
+set_data = {1,2,3,4,5,6,7,8}
+print(set_data)
+for x in set_data:
+    print(x)
+# Count not supported in set
+# Index not supported in set
+
+print("Maximum Number:")
+print(max(set_data))
+
+print("Minimum Number:")
+print(min(set_data))
+
+print("Sum of Numers")
+print(sum(set_data))
+
+print("Calculate lenght")
+print(len(set_data))
