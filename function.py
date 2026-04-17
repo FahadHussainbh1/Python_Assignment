1d6c20e0a4254bc1f3af1ef5905920bb2cb372b0
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-17 11:44:18 +0500

    Add files via upload
    python functions

---
+++ function.py
@@ -0,0 +1,44 @@
+# def the_function():
+#     print("You have created a function")
+
+# the_function()
+
+
+# def a_function():
+#     print("New funtion")
+
+# a_function()
+
+
+# def loop_function():
+#     for x in range(1,11):
+#         print(x)
+# loop_function()        
+
+
+
+# def loop_function():
+#     for x in range(1,input):
+#         print(x)
+# input = int(input("Enter a number: "))        
+# loop_function()        
+
+
+# def loop_function():
+#     for x in range(1,input+1):
+#         print(x)
+# input = int(input("Enter a number: "))        
+# loop_function()        
+
+
+# def loop(num1,num2):
+#     print("sum is :" , num1+num2)
+# loop(50,50)    
+
+
+# def loop(num1,num2):
+#     print("Multiplication :" , num1*num2)
+# loop(5,5)    
+
+
+
---
+++ functions.py
@@ -0,0 +1,39 @@
+# def add(num1,num2):
+#     sum = num1+num2
+#     return sum
+
+# print(sum)
+
+
+# def isEven(num):
+#     if num%2==0:
+#         return True
+#     else:
+#         return False
+
+# print(isEven(9))
+
+
+# A function that create Even and Odd numbers
+
+# def odd(num):
+#     if num%2==0:
+#         return "Even"
+#     else:
+#         return "Odd"
+    
+# print(odd(11))
+
+
+# A function ,that divisable by 5
+
+def num1(num):
+    if num%5==0:
+        return num,"is divisable by 5"
+    else:
+        return num,"is not divisable by 5" 
+
+print(num1(11))     
+
+
+    
