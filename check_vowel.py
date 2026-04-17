e4d61a52106c635fe28f57985e3d568d4a44ed80
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-17 12:22:59 +0500

    Add files via upload
    python tasks

---
+++ check_vowel.py
@@ -0,0 +1,16 @@
+# A function that create vowel or consonat alphabet 
+def word(ch):
+    if ch == "a":
+        return "vowel"
+    elif ch == "e":
+        return "vowel"
+    elif ch == "i":
+        return "vowel"
+    elif ch == "o":
+        return "vowel"
+    elif ch == "u":
+        return "vowel" 
+    else:
+        return "consonant"
+    
+print(word("a"))     
---
+++ divisable.py
@@ -0,0 +1,10 @@
+
+# A function ,that divisable by 5
+
+def num1(num):
+    if num%5==0:
+        return num,"is divisable by 5"
+    else:
+        return num,"is not divisable by 5" 
+
+print(num1(11))     
