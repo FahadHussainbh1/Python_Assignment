c41cb52b68b3293f50d6c02d23fa752a42edd617
Fahad Hussain <fahadhussainbh99@gmail.com>
2026-04-20 14:51:55 +0500

    Add files via upload
    class tasks

---
+++ dog.py
@@ -0,0 +1,33 @@
+class Dog:
+    name = "Tommy"
+    age  = "9 years"
+    breed = "german shephard"
+
+    def __init__(self):
+        print("The Dog")
+
+    def Bark():
+        print("Barking on undefind humans")
+
+    def Eat():
+        print("Chicken,Fish,Beef")
+
+    def Sleep():
+        print("12-14 Houres")
+
+
+    # Bark()
+    # Eat()
+    # Sleep()
+
+shephard = Dog()
+print(shephard.name)
+print(shephard.age)
+print(shephard.breed)
+
+print()
+    
+bull = Dog()
+print(shephard.name)
+print(shephard.age)
+print(shephard.breed)
---
+++ object_computer.py
@@ -0,0 +1,16 @@
+class Computer:
+    name = "Cor i7"
+    generation = "14th"
+
+
+
+def Dislpay():
+    print("display screen")        
+
+
+computer = Computer()
+print(computer.name)
+print(computer.generation)
+
+
+
